e2759c839283c7c6f6121fab56e39ddb98dd42b7
Michelle Tan <mtan5334@uw.edu>
2017-01-10 15:27:03 -0800

    module 6 exercise 3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,28 +1,29 @@
 # Exercise-3: Calling built-in functions
 
 # Create a variable `my.name` that contains your name
+my.name <- "Michelle Tan"
 
-
-# Create a variable `name.length` that holds how many letters are in your name (use the `nchars()` function)
-
+# Create a variable `name.length` that holds how many letters are in your name (use the `nchar()` function)
+name.length <- nchar(my.name)
 
 # Print the number of letters in your name
-
+print(name.length)
 
 # Create a variable `now.doing` that is your named followed by "is programming!" to the end of your name
 # (use the `paste()` function)
-
+now.doing <- paste (my.name, "is programming!")
 
 # Make the `now.doing` variable upper case
-
+toupper(now.doing)
 
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to `fav.1` and `fav.2`
-
+fav.1 <- 7
+fave.2 <- 1
 
 # Divide each number by the square root of 201 and save it as the same variable
-
+square.rooted.fav.numbers <-
 
 # Create a variable `raw.sum` that is the sum of those two variables. Use the `sum()` function for practice
 
